8a540fc58594b890a0b1b86949599a81fede6068
dylanxcsl <dylanxcsl@gmail.com>
2024-02-18 00:37:42 -0500

    Update 10-controls.R

--- code/nom7-total/10-controls.R
+++ code/nom7-total/10-controls.R
@@ -7,10 +7,10 @@ library(wnominate)
 View(vview.final)     ## Table with constructed nom7 score for every legislator (tenure not included)
 View(vview.unique)    ## Table with constructed nom7 score for every legislator (tenure included)
 View(vview.median)    ## Median of constructed nom7 score for each party per Congress
+View(vview.long)      ## Median of constructed nom7 & dim 1 scores for each party per Congress
 
 # Visualizations
 scatter_legis       ## Scatterplot of all legislators (x = dim1, y = nom7)
 boxplot_legis       ## Boxplot of all legislators (x = dim1, y = nom7)
 scatter_median.nom7 ## Scatterplot of party nom7 median (x = cong, y = nom7)
 scatter_median.all  ## Scatterplot of party nom7 median & dim 1 median
-
